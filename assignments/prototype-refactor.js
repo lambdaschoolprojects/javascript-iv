b680538a0e330b4e45c9917999ce3b01a7a37c6d
Rane Wallin <RAN2143066@maricopa.edu>
2019-01-23 21:08:24 -0700

    prototype-refactor mvp

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -183,7 +183,7 @@ console.log(mage.weapons); // Staff of Shamalama
 console.log(archer.language); // Elvish
 console.log(archer.greet()); // Lilith offers a greeting in Elvish.
 console.log(mage.takeDamage()); // Bruce took damage.
-console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.           
 
 
 // Stretch task:
